fc8d7e876f5f1a261991a72beab9d79acaa9c252
lipegomes <fgdl.py91@gmail.com>
2020-10-22 16:02:43 -0300

    Fix.

--- pages/contato.js
+++ pages/contato.js
@@ -9,8 +9,8 @@ const Contato = () => {
         <p className='text-center font-bold py-4'>
           Entre em contado através de:
         </p>
-        <div className='container text-center'>
-          <a className='mb-8 text-center px-20' href='https://www.linkedin.com/in/filipe-gomes-43905a1b2/'>Linkedin: https://www.linkedin.com/in/filipe-gomes-43905a1b2/</a>
+        <div className='container mb-8 text-center'>
+          <a className='text-center px-20' href='https://www.linkedin.com/in/filipe-gomes-43905a1b2/'>Linkedin: https://www.linkedin.com/in/filipe-gomes-43905a1b2/</a>
         </div>
       </div>
     </div>

5c149abcb5a5d77d7184cf928befecf84bd94a0a
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2020-10-28 18:57:55 -0300

    Update linkedin href in footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='container mx-auto text-center font-bold text-white'>
         Projeto desenvolvido por: {' '}
         <a className='hover:underline' href='https://filipegomes.dev'>Filipe Gomes</a> / {' '}
-        <a className='hover:underline' href='https://www.linkedin.com/in/filipe-gomes-43905a1b2/'>Linkedin</a> / {' '}
+        <a className='hover:underline' href='https://www.linkedin.com/in/lipegomes/'>Linkedin</a> / {' '}
         <a className='hover:underline' href='https://github.com/lipegomes'>Github</a>
         <div className='mt-2'>
           <img className='inline p-4' width='214' height='75' src='/logo_semana_fsm.png' />
